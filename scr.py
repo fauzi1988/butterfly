a7329fe92d8c01fbf3c25156ce4b9ceab1fd5c40
Fauzi <61728637+fauzi1988@users.noreply.github.com>
2021-11-13 16:13:04 +0700

    Streamlit & Model File

---
+++ scr.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c17bc9ade186d145c6afb5606c2b27d215e35df24b9666f2f64b825e3c71b4b1
+size 97
---
+++ streamlit_host.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:701d4f4e58c4b78485702a9a9aee243ecdf51718ee8346d3308dd97928bece92
+size 1496
